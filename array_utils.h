766a672798bed16ef51ba7144b406f7ef034ab2c
naseemshahid <naseemshahid69@gmail.com>
2023-12-20 05:22:31 +0500

    prottotypes

--- array_utils.h
+++ array_utils.h
@@ -1,3 +1,23 @@
 
 
 //place your documentation and prototypes here
+#include<stdio.h>
+#define true 1
+#define false 0
+
+//Q1 :function prototype
+ int contains(const int *arr, int size, int x);
+
+//Q2 :function prototype
+int containsWithin( int *arr, int size, int x, int i, int j);
+
+// Q4 :function prototype
+void reverse(int *arr, int size);
+
+// Q3 :function prototype
+int *paddedCopy(const int *arr, int oldSize, int newSize,int *newarr);
+
+// Q5 :function prototype
+int * reverseCopy(int *arr, int size);
+
+
